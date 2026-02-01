65c7fcea7314c37f821f46d06cdbcf1070030cc2
SRINUNAIK208 <srinaik208@gmail>
2026-02-01 15:06:58 +0530

    terraform

--- output.tf
+++ output.tf
@@ -4,3 +4,9 @@ output "vpc_id"{
 output "public_subnet_id" {
     value = aws_subnet.public[*].id
 }
+output "private_subnet_id" {
+    value = aws_subnet.private[*].id
+}
+output "database_subnet_id" {
+    value = aws_subnet.database[*].id
+}
